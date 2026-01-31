3d71abc4b26300a350899459f8bee280caea2db9
svetlana-kolesnikova <sv.ps1286@gmail.com>
2026-01-31 21:55:07 +0300

    add urls.py, views.py,

---
+++ network/urls.py
@@ -0,0 +1,11 @@
+# network/urls.py
+
+from rest_framework.routers import DefaultRouter
+from .views import NetworkNodeViewSet
+
+
+router = DefaultRouter()
+router.register("nodes", NetworkNodeViewSet, basename="nodes")
+
+
+urlpatterns = router.urls
--- network/views.py
+++ network/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# network/views.py
 
-# Create your views here.
+from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import NetworkNode
+from .serializers import NetworkNodeSerializer
+
+
+class NetworkNodeViewSet(ModelViewSet):
+    """CRUD для звена сети с фильтрацией по стране."""
+
+    queryset = NetworkNode.objects.select_related("contact", "supplier").all()
+    serializer_class = NetworkNodeSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ("contact__country",)
